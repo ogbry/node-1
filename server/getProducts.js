75adb011887806ab5a6600b371ff9de7f208f150
ogbry <tengenexd@gmail.com>
2019-12-19 14:36:18 +0800

    patch

--- server/getProducts.js
+++ server/getProducts.js
@@ -22,12 +22,45 @@ function getProductById(req, res){
 }
 
 function postProduct(req, res){
-  console.log(req.body)
+  	products.push(req.body)
+	res.status(200).json(products)
+}
+
+function editProduct(req, res){
+	products.forEach(item => {
+         if(parseInt(req.params.id) === item.id){
+                item.product_name = req.body.product_name,
+                item.price = req.body.price ? req.body.price : item.price
+                item.img_url = req.body.img_url ? req.body.img_url : item.img_url
+         }
+        })
+}
+
+function deleteProduct(req, res){
+	let index = parseInt(req.params.id)
+
+        products.forEach(item => {
+
+        if(index === item.id){
+        const item = products.indexOf(products.find( x => {
+                if(x.id == index){
+                        return x
+                }
+        }))
+        products.splice(item, 1)
+        }
+        else{
+                console.log('Id not found')
+        }
+        })
+
 }
 
 module.exports = {
   getProducts,
   getProductById,
-  postProduct
+  postProduct,
+  editProduct,
+  deleteProduct
 
 }
--- server/index.js
+++ server/index.js
@@ -16,39 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/api/products', getProduct.getProducts)
 app.get('/api/products/:id', getProduct.getProductById) 
-app.post('/api/products/add', function(req, res){ 
-	products.push(req.body)
-        res.status(200).json(products)
-     })
-app.patch('/api/products/edit/:id', function(req, res){
-	products.forEach(item => {
-	 if(parseInt(req.params.id) === item.id){
-		item.product_name = req.body.product_name,
-		item.price = req.body.price ? req.body.price : item.price
-		item.img_url = req.body.img_url ? req.body.img_url : item.img_url
-	 }
-	})
-})
-app.delete('/api/products/delete/:id', function(req, res){
-	let index = parseInt(req.params.id)
-
-	products.forEach(item => {
-
-	if(index === item.id){
-	const item = products.indexOf(products.find( x => {
-		if(x.id == index){
-			console.log(x)
-			return x
-		}
-	}))
-	products.splice(item, 1)
-	}
-	else{
-		console.log('Id not found')
-	}
-	})
-
-	
-})
-
+app.post('/api/products/add', getProduct.postProduct)
+app.patch('/api/products/edit/:id', getProduct.editProduct)
+app.delete('/api/products/delete/:id', getProduct.deleteProduct)
 app.listen( port, (error) => {  console.log(`Server is listening on port ${port} c((•ω•))ɔ`)  })
